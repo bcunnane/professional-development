86f8514cf928bc7d05cb5306f5ad47d3d30e86e3
bcunnane <brandon.cunnane@gmail.com>
2022-10-04 15:35:02 -0700

    added comments

--- Python Data Structures and Algorithms/brandonCunnaneLab1.py
+++ Python Data Structures and Algorithms/brandonCunnaneLab1.py
@@ -1,4 +1,7 @@
-
+"""
+Movie class
+Maintains a list of movie titles and release years
+"""
 
 class Movie:
     
@@ -17,12 +20,14 @@ class Movie:
     
     # mutators
     def setName(self, the_name):
+        # set movie name
         if Movie.strOK(self, the_name):
             self.name = the_name
         else:
             self.name = Movie.DEFAULT_NAME
     
     def setYear(self, the_year):
+        # set movie year
         if Movie.yearOK(self, the_year):
             self.year = the_year
         else:
@@ -44,6 +49,7 @@ class Movie:
         return self.year
     
     def getList():
+        # display a numbered list of all recorded movie titles and years
         count = 1
         for item in Movie._movieList:
             print(f'{count}. {item}')
@@ -53,9 +59,11 @@ class Movie:
         
     # helpers
     def getStr(self):
+        # returns a string of the movie title and year
         return str(self)
     
     def strOK(self, the_str):
+        # checks if an input string is less than 50 characters
         MAX_LEN = 50
         if len(the_str) <= MAX_LEN:
             return True
@@ -63,6 +71,7 @@ class Movie:
             return False
     
     def yearOK(self, the_year):
+        # checks if an input year is valid, between years 1000 and 2023
         MIN_YEAR = 1000
         MAX_YEAR = 2023
         if MIN_YEAR < the_year < MAX_YEAR:
--- Python Data Structures and Algorithms/brandonCunnaneLab1demo.py
+++ Python Data Structures and Algorithms/brandonCunnaneLab1demo.py
@@ -1,28 +1,105 @@
+"""
+Operates a menu for user interaction with the Movie class
+"""
 
 from brandonCunnaneLab1 import Movie
 
-menu = """COMMAND MENU
+def main():
+    menu = """COMMAND MENU
+    list - List all movies
+    add -  Add a movie
+    del -  Delete a movie
+    exit - Exit program
+    """
+    print(menu)
+    while True:
+        command = input("Command: ")
+        if command == 'exit':
+            print('Bye!')
+            break
+        elif command == 'list':
+            Movie.getList()
+        elif command == 'add':
+            name = input('Name: ')
+            year = int(input('Year: '))
+            Movie.add(Movie(name, year))
+        elif command == 'del':
+            movie_num = int(input('Number: '))
+            Movie.delete(movie_num)
+        else:
+            print('Invalid command.\n')
+
+
+if __name__ == '__main__':
+    main()
+
+
+"""
+COMMAND MENU
 list - List all movies
 add -  Add a movie
 del -  Delete a movie
 exit - Exit program
+
+Command: add
+Name: Ladybird
+Year: 2018
+Ladybird was added.
+
+Command: add
+Name: Call me by your name
+Year: 2017
+Call me by your name was added.
+
+Command: add
+Name: In the Heights
+Year: 2021
+In the Heights was added.
+
+Command: add
+Name: Dune
+Year: 2021
+Dune was added.
+
+Command: add
+Name: The Batman
+Year: 2022
+The Batman was added.
+
+Command: list
+1. Ladybird (2018)
+2. Call me by your name (2017)
+3. In the Heights (2021)
+4. Dune (2021)
+5. The Batman (2022)
+
+Command: add
+Name: Little Miss Sunshine
+Year: 2007
+Little Miss Sunshine was added.
+
+Command: list
+1. Ladybird (2018)
+2. Call me by your name (2017)
+3. In the Heights (2021)
+4. Dune (2021)
+5. The Batman (2022)
+6. Little Miss Sunshine (2007)
+
+Command: del
+Number: 3
+In the Heights was deleted.
+
+Command: list
+1. Ladybird (2018)
+2. Call me by your name (2017)
+3. Dune (2021)
+4. The Batman (2022)
+5. Little Miss Sunshine (2007)
+
+Command: abc
+Invalid command.
+
+Command: exit
+Bye!
 """
-print(menu)
-while True:
-    command = input("Command: ")
-    if command == 'exit':
-        print('Bye!')
-        break
-    elif command == 'list':
-        Movie.getList()
-    elif command == 'add':
-        name = input('Name: ')
-        year = int(input('Year: '))
-        Movie.add(Movie(name, year))
-    elif command == 'del':
-        movie_num = int(input('Number: '))
-        Movie.delete(movie_num)
-    else:
-        print('Invalid command.\n')
-        
-    
